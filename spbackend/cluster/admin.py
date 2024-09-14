b28d8a7a36f464304957a6c9aa23642d5ae87748
Trance-0 <60459821+Trance-0@users.noreply.github.com>
2024-09-14 00:00:23 -0500

    Update admin.py
    done basic editing

--- spbackend/cluster/admin.py
+++ spbackend/cluster/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category,Development,Page,Section,Relation,Tag
+
+class SectionInline(admin.StackedInline):
+    """Line per message in admin view and one extra for convenience"""
+    model=Section
+    ordering=["date_created"]
+    readonly_fields=["date_created","last_edit"]
+    extra=1
+
+class PageAdmin(admin.ModelAdmin):
+    """ in admin view and one extra for convenience"""
+    model=Page
+    readonly_fields=["date_created","last_edit"]
+    inlines = [
+        SectionInline
+    ]
+    extra=1
+
+
+# Add model to admin view
+admin.site.register(Category)
+admin.site.register(Development)
+admin.site.register(Page,PageAdmin)
+admin.site.register(Relation)
+admin.site.register(Tag)
