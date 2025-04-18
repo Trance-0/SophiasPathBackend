fd49dc09ff9883ebae2ca4c95e1d7105213aea53
Zheyuan Wu <60459821+Trance-0@users.noreply.github.com>
2025-04-18 18:53:08 -0500

    upgrade django version and docker settings

--- spbackend/entrypoint.sh
+++ spbackend/entrypoint.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
+
 # if the script cannot be found, change the file from CRLF to LF
+
+# create .env file from environment variables, for python settings
+echo "Creating .env file from environment variables..."
+
+cat > .env <<EOF
+# secret key for website
+SECRET_KEY=$SECRET_KEY
+# database setting
+DATABASE=$DATABASE
+POSTGRE_USERNAME=$POSTGRE_USERNAME
+POSTGRE_PASSWORD=$POSTGRE_PASSWORD
+POSTGRE_HOST=$POSTGRE_HOST
+POSTGRE_PORT=$POSTGRE_PORT
+# initial user setting
+DJANGO_SUPERUSER_USERNAME=$DJANGO_SUPERUSER_USERNAME
+DJANGO_SUPERUSER_EMAIL=$DJANGO_SUPERUSER_EMAIL  
+DJANGO_SUPERUSER_PASSWORD=$DJANGO_SUPERUSER_PASSWORD
+# allow hosts for website
+ALLOWED_HOSTS=$ALLOWED_HOSTS
+# log file config, the log file name should be in (relative path)
+DJANGO_LOG_FILE_NAME=$DJANGO_LOG_FILE_NAME
+DJANGO_LOG_LEVEL=$DJANGO_LOG_LEVEL
+
+# debug parameter (onetime)
+DEBUG=$DEBUG
+
+# nginx settings
+NGINX_PORT=$NGINX_PORT
+DJANGO_PORT=$DJANGO_PORT
+
+# media and static root for production (absolute path), also need to be configured in nginx settings
+PRODUCTION_MEDIA_ROOT=$PRODUCTION_MEDIA_ROOT
+PRODUCTION_STATIC_ROOT=$PRODUCTION_STATIC_ROOT
+EOF
+
+echo ".env file created successfully."
+
+# rewrite nginx settings
 
 # laod env variables: https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-value-pairs#comment37343914_20909045
 export $(grep -v '^#' .env | xargs)
@@ -7,7 +46,7 @@ export $(grep -v '^#' .env | xargs)
 if [ "$DATABASE" = "postgres" ]
 then
     echo "Waiting for postgres..."
-    echo "http://" $POSTGRE_HOST $POSTGRE_PORT
+    echo "http://" $POSTGRE_HOST ":" $POSTGRE_PORT
 
     while ! nc -z $POSTGRE_HOST $POSTGRE_PORT; do
       sleep 0.1
